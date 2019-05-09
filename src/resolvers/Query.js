c3e9f5205f777c9561836a5a87bed3589e34aa3d
Muhammad Furqan <furqan.khanzada@gmail.com>
2019-05-09 07:09:58 +0500

    basic filtration on customers implemented

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,12 @@
 
 const Query = {
-    me(parent, args, { user : { id }, prisma }) {
+    me(parent, { where = {}}, { user : { id }, prisma }) {
         return prisma.user({ id })
     },
     customers(parent, args, { prisma }) {
         return prisma.users({
             where: {
+                ...args.where,
               role: 'CUSTOMER'
             }
         })
