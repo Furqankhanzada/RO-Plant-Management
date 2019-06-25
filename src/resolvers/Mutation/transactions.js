4dffc9437a0de31aff4ab3ab5319b899447561c3
muhazzib <muhazzib7@gmail.com>
2019-06-25 18:53:43 +0500

    done with bottle balance task

--- src/resolvers/Mutation/transactions.js
+++ src/resolvers/Mutation/transactions.js
@@ -1,15 +1,44 @@
 
 const transactions = {
   async createTransaction(parent, args, context) {
-    return await context.prisma.createTransaction({ ...args.data });
+    const { data:{user:{connect:{id}}} } = args;
+    const result = await context.prisma.createTransaction({ ...args.data })
+    await context.prisma.updateUser({
+      data: {
+        bottle:{
+          update:{
+            balance: args.bottleBalance
+          }
+        }
+      },
+      where: {
+        id
+      }
+    });
+    return result
+    
   },
   async updateTransaction(parent, args, context) {
-    return await context.prisma.updateTransaction({
+    const { userID } = args;
+    const result = await context.prisma.updateTransaction({
       data: args.data,
       where: {
         ...args.where
       }
     });
+    await context.prisma.updateUser({
+      data: {
+        bottle:{
+          update:{
+            balance: args.bottleBalance
+          }
+        }
+      },
+      where: {
+        id: userID
+      }
+    });
+    return result
   },
   async deleteTransaction(parent, args, context) {
     return await context.prisma.deleteTransaction({ ...args.where });
